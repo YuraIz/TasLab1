4066d62510fb13b36d27fb7182f26f3c5848d8e8
YuraIz <7516890@gmail.com>
2021-02-18 16:57:14 +0300

    Update

--- Lab 1/Program.cs
+++ Lab 1/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections;
 
 namespace Lab_1
 {
+    class Test
+    {
+        private string number;
+
+        public Test(string num)
+        {
+            number = num;
+        }
+        public void Check(string b)
+        {
+            if (number == b)
+            {
+                Console.WriteLine("Yeah");
+            }
+            else
+            {
+                Console.WriteLine("Nope");
+            }
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main()
+        {   
+            Test num = new Test("2048");
+            
+            Console.WriteLine("Try to guess a number");
+            for (var i = 0; i < 5; i++)
+            {
+                var number = Console.ReadLine();
+                num.Check(number);
+            }
         }
     }
 }
